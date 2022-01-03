60f5ac254341f810c00f0ccdd2cdde6c470e2e1c
Eric Matlock <ermatlock@gmail.com>
2022-01-02 23:54:10 -0500

    Refactor for submittal

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var choiceMantra = document.getElementById("choice-mantra");
 var buddhaIcon = document.querySelector(".meditate-svg");
 var messageResult = document.getElementById("message-result");
 var messageButton = document.getElementById("receive-message");
-var clearButton = document.getElementById("clear");
-var loader = document.querySelector(".loader")
+var clearButton = document.getElementById("clear-button");
+var loader = document.querySelector(".loader");
 
 /* ~~~~~~~~~~~~~~~~ Event Listeners ~~~~~~~~~~~~~~~~~*/
 messageButton.addEventListener("click", getMessage);
@@ -17,54 +17,52 @@ messageButton.disabled = true;
 
 /* ~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~*/
 function show(element) {
-  element.classList.remove("hidden");
+	element.classList.remove("hidden");
 };
 
 function hide(element) {
-  element.classList.add("hidden");
+	element.classList.add("hidden");
 };
 
 function fadeIn(element) {
-  element.classList.add("fade-in");
-  element.classList.remove("hidden");
-}
+	element.classList.add("fade-in");
+	element.classList.remove("hidden");
+};
 
 function getRandom(array) {
-  return Math.floor(Math.random() * array.length);
+	return Math.floor(Math.random() * array.length);
 };
 
 function runTimer(functionName, element, time) {
-  setTimeout(function(){functionName(element)}, time);
-};
-
-function selectChoice() {
-  if (choiceAffirmation.checked) {
-    messageResult.innerText = `${affirmations[getRandom(affirmations)]}`;
-  } else if (choiceMantra.checked) {
-    messageResult.innerText = `${mantras[getRandom(mantras)]}`;
-  }
-};
-
-function getMessage() {
-  hide(buddhaIcon);
-  hide(messageResult);
-  hide(clearButton);
-  show(loader);
-  selectChoice();
-  runTimer(hide, loader, 500);
-  runTimer(fadeIn, messageResult, 500);
-  runTimer(fadeIn, clearButton, 500);
-};
-
-function clearMessage() {
-  hide(messageResult)
-  hide(clearButton)
-  fadeIn(buddhaIcon)
-
-
+	setTimeout(function() {functionName(element)}, time);
 };
 
 function activateButton() {
-  messageButton.classList.remove("inactive")
-  messageButton.disabled = false;
-}
+	messageButton.classList.remove("inactive");
+	messageButton.disabled = false;
+};
+
+function selectChoice() {
+	if (choiceAffirmation.checked) {
+		messageResult.innerText = `${affirmations[getRandom(affirmations)]}`;
+	} else if (choiceMantra.checked) {
+		messageResult.innerText = `${mantras[getRandom(mantras)]}`;
+	}
+};
+
+function getMessage() {
+	hide(buddhaIcon);
+	hide(messageResult);
+	hide(clearButton);
+	show(loader);
+	selectChoice();
+	runTimer(hide, loader, 500);
+	runTimer(fadeIn, messageResult, 500);
+	runTimer(fadeIn, clearButton, 500);
+};
+
+function clearMessage() {
+	hide(messageResult);
+	hide(clearButton);
+	fadeIn(buddhaIcon);
+};
